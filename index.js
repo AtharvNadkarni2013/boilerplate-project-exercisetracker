7c8d1069c6f98e1c28e1536bfd4a5578bf4a042e
AtharvNadkarni2013 <81621785+AtharvNadkarni2013@users.noreply.github.com>
2024-05-27 22:48:36 +0530

    code

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const User = mongoose.model("User", userSchema);
 const exerciseSchema = new mongoose.Schema({
   user_id: String,
   username: String,
-  date: String,
+  date: Date,
   duration: Number,
   description: String,
 });
@@ -31,14 +31,54 @@ const Exercise = mongoose.model("Exercise", exerciseSchema);
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use((req, res, next) => {
+  console.log(req.url, req.method, req.body, req.params)
+  next()
+})
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+app.get('/api/users', (req, res) => {
+  User.find({}, (err, data) => {
+    res.json(data)
+  })
+})
+
+app.get('/api/users/:_id/logs', (req, res) => {
+  const { _id } = req.params;
+  const { from, to, limit } = req.query
+  let response = {};
+  User.findById(_id).exec((err, userData) => {
+    if (!userData) {
+      return res.json({error: "no user data"})
+    }
+    response = userData._doc
+    let findBy = {user_id: _id}
+    if (from) {
+      findBy = Object.assign({}, findBy, {date: {$gt: new Date(from)}})
+    }
+    if (to) {
+      findBy = Object.assign({}, findBy, {date: {$lt: new Date(to)}})
+    }
+    console.log(findBy)
+    Exercise.find(findBy).select({description: 1, duration: 1, date: 1, _id: 0}).limit(parseInt(limit)).exec((err, exerciseData) => {
+      exerciseData.forEach(dat => {
+        dat._doc.date = new Date(dat.date).toDateString()
+      })
+      response.count = exerciseData.length
+      response.log = exerciseData
+      res.json(response)
+    })
+  })
+})
 
 app.post("/api/users", (req, res) => {
   const { username } = req.body;
   const newUser = new User({ username });
   newUser.save((err, user) => {
+    if (err) return console.error(err)
     res.json(user);
   });
 });
@@ -53,7 +93,12 @@ app.post("/api/users/:_id/exercises", (req, res) => {
       if (!user) return res.status(404).json({ error: "User does not exist" });
 
       username = user.username;
-      const date = new Date(req.body.date);
+      let date;
+      if (req.body.date) {
+        date = new Date(req.body.date).toDateString();
+      } else {
+        date = new Date().toDateString();
+      }
       const duration = parseInt(req.body.duration);
       const newExercise = new Exercise({
         user_id: userId,
@@ -68,6 +113,7 @@ app.post("/api/users/:_id/exercises", (req, res) => {
         delete data._doc.user_id
         delete data._doc.__v
         data._doc._id = userId
+        data._doc.date = new Date(data.date).toDateString();
         res.json(data);
       });
     });
